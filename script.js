d0af852a30ad00f5980cfabc650f884c890c7659
Drallas <24792888+Drallas@users.noreply.github.com>
2023-10-31 11:59:29 +0100

    new search engine

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchBox = document.getElementById('search')
 searchBox.addEventListener('keydown', (e) => {
   if (e.code == 'Enter') {
     const { value } = searchBox
-    if (value.replace(/ /gm, '').length > 0) location.href = `https://www.ecosia.org/search?q=${value}`
+    if (value.replace(/ /gm, '').length > 0) location.href = `https://whoogle.soholocal.nl/search?q=${value}`
     else searchBox.value = ''
   }
 })
@@ -11,3 +11,6 @@ searchBox.addEventListener('keydown', (e) => {
 window.onload = () => {
   document.getElementsByTagName('body')[0].removeAttribute('class')
 }
+
+# https://www.ecosia.org/search?q=${value}
+
